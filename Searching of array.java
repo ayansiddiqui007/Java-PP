e6813fecbbc23bb587b01c1a8260d25157c4ebd6
AYAN SIDDIQUI <52853276+ayansiddiqui007@users.noreply.github.com>
2022-08-03 20:47:00 +0530

    Searching array PRO 3

---
+++ Searching of array.java
@@ -0,0 +1,21 @@
+class Greek_Gods
+{
+	public static void main (String[] args)
+	{
+	
+	// Creating an integer array
+	// named arr of size 10.
+	int[] arr = new int[10];
+	
+	// accessing element at 0 index
+	// and setting its value to 5.
+	arr[0] = 5;
+	
+	// access and print value at 0
+	// index we get the output as 5.
+	System.out.println(arr[0]);
+		
+	}
+}
+
+// This code is contributed by muditj148.
